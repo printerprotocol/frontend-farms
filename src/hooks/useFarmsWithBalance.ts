6a8740434bae99c1d13a87a8edf5fd3a03913231
PrinterDev <80286557+printerprotocol@users.noreply.github.com>
2021-03-17 21:52:26 -0300

    testnet farming app

--- src/hooks/useFarmsWithBalance.ts
+++ src/hooks/useFarmsWithBalance.ts
@@ -21,7 +21,7 @@ const useFarmsWithBalance = () => {
     const fetchBalances = async () => {
       const calls = farmsConfig.map((farm) => ({
         address: getMasterChefAddress(),
-        name: 'pendingEgg',
+        name: 'pendingPts',
         params: [farm.pid, account],
       }))
 
--- src/views/Home/components/TwitterCard.tsx
+++ src/views/Home/components/TwitterCard.tsx
@@ -35,7 +35,7 @@ const TwitterCard = () => {
         <Timeline
           dataSource={{
             sourceType: 'profile',
-            screenName: 'GooseFinance'
+            screenName: 'PrinterFinance'
           }}
           options={{
             height: '300',
